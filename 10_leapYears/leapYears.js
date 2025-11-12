4055053fc1290a673aa8f20a10fe0977dbfc67cf
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-12 15:50:09 -0400

    Complete exercise 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const yearIsMultipleOf4 = (year % 4 === 0);
+    const yearIsMultipleOf100 = (year % 100 === 0);
+    const yearIsMultipleOf400 = (year % 400 === 0);
 
+    yearIsLeapYear = yearIsMultipleOf4 && (!yearIsMultipleOf100 || yearIsMultipleOf400);
+
+    return yearIsLeapYear;
 };
 
 // Do not edit below this line

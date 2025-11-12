d187b8bd3f50e29d42f472ccc5b12312a987746f
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-12 15:42:19 -0400

    Complete exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+    
+    const start = a <= b ? a : b;
+    const end = a >= b ? a : b;
+    
+    let sum = 0;
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

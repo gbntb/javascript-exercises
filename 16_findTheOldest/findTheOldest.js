1e246cd6bf190d297e0b30f24cfa06b1db3d57ec
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-15 12:55:18 -0400

    Complete exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
 
+    array.sort((a, b) => {
+        ageA = ("yearOfDeath" in a) ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+        ageB = ("yearOfDeath" in b) ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
+        return ageB - ageA;
+    });
+
+    return array[0];
 };
 
 // Do not edit below this line

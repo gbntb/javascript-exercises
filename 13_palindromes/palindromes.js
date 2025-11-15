29a142ecf9544261f3b7e272dc36b8ec53e3d0fd
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-15 11:27:08 -0400

    Complete exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    let alphanumericStringArray = string.toLowerCase().split("").filter((char) => alphanumeric.includes(char));
+    return alphanumericStringArray.join("") === alphanumericStringArray.reverse().join("");
 
 };
 
